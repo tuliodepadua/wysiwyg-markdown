2bb805a17310ad5383e68c7fd19a2cf24f4311c1
Túlio de Pádua <tuliodepadua@gmail.com>
2018-08-30 16:49:32 -0300

    Array menu

--- dist/wysiwyg-markdown.js
+++ dist/wysiwyg-markdown.js
@@ -32,7 +32,7 @@ wmh.geti = (tipo) => {
             break;
         case 'html':
             vlr = $('#' + wmh.dv).val().replace(/\n/g, "<br />");
-            
+
             markdown = turndownService.turndown(vlr)
             vlr = markdown;
             wmh.backHTML(vlr);
@@ -43,47 +43,87 @@ wmh.geti = (tipo) => {
     }
 }
 
-function iniWHM(vlr, style) {
+function iniWHM(vlr, style, arr) {
+    console.log(arr)
+    let ar = wmh.menu(arr);
+
+    console.log(ar);
+
     if (style == undefined) {
         style = "";
     }
+
     let vlrinp = $('#' + vlr);
+
     vlrinp = vlrinp[0].parentElement.id;
-    $('#' + vlrinp).append(wmh.menu);
+
+    $('#' + vlrinp).append(ar);
+
     wmh.dv = vlr;
+
     editor = new wysihtml5.Editor(wmh.dv, {
         toolbar: "wmh",
         stylesheets: style,
         parserRules: wysihtml5ParserRules
     });
 }
+wmh.menu = (options) => {
+    let arr = '<div id="wmh">';
+    for (let i = 0; i < options.length; i++) {
+        switch (options[i]) {
+            case 'b':
+                arr += "<a class='btn' data-wysihtml5-command='bold'  title='CTRL + U'>  <i class='material-icons'>format_bold</i></a>";
+                break;
+            case 'i':
+                arr += "<a class='btn' data-wysihtml5-command='italic'  title='CTRL+i'><i class='material-icons'>format_italic</i></a>";
+                break;
+            case 'flb':
+                arr += "<a class='btn' data-wysihtml5-command='insertUnorderedList'><i class='material-icons'>format_list_bulleted</i></a>";
+                break;
+            case 'fln':
+                arr += "<a class='btn' data-wysihtml5-command='insertOrderedList'><i class='material-icons'>format_list_numbered</i></a>";
+                break;
+            case 'h1':
+                arr += "<a class='btn' data-wysihtml5-command='formatBlock'  data-wysihtml5-command-value='h1'>h1</a>";
+                break;
+            case 'h2':
+                arr += "<a class='btn' data-wysihtml5-command='formatBlock'  data-wysihtml5-command-value='h2'>h2</a>";
+                break;
+            case 'faLeft':
+                arr += "<a class='btn' data-wysihtml5-command='justifyLeft' ><i class='material-icons'>format_align_left</i></a>";
+                break;
+            case 'faCenter':
+                arr += "<a class='btn' data-wysihtml5-command='justifyCenter' ><i class='material-icons'>format_align_center</i></a>";
+                break;
+            case 'faRight':
+                arr += "<a class='btn' data-wysihtml5-command='justifyRight' ><i class='material-icons'>format_align_right</i></a>";
+                break;
 
-wmh.menu = `<div id="wmh">
-                <a class='btn' data-wysihtml5-command="bold"  title='CTRL+U'>  <i class="material-icons">format_bold</i></a>
-                <a class='btn' data-wysihtml5-command="italic"  title='CTRL+i'><i class="material-icons">format_italic</i></a>    
-                <a class='btn' data-wysihtml5-command="insertUnorderedList"><i class="material-icons">format_list_bulleted</i></a>
-                <a class='btn' data-wysihtml5-command="insertOrderedList"><i class="material-icons">format_list_numbered</i></a>
-                <a class='btn' data-wysihtml5-command="formatBlock"  data-wysihtml5-command-value="h1">h1</a>
-                <a class='btn' data-wysihtml5-command="formatBlock"  data-wysihtml5-command-value="h2">h2</a>
-                <a class='btn' data-wysihtml5-command="justifyLeft" ><i class="material-icons">format_align_left</i></a>
-                <a class='btn' data-wysihtml5-command="justifyCenter" ><i class="material-icons">format_align_center</i></a>
-                <a class='btn' data-wysihtml5-command="justifyRight" ><i class="material-icons">format_align_right</i></a>
-            </div>`;
+            default:
+                break;
+        }
+
+    }
+    arr += '</div>';
+    return arr;
+}
 
 
 wmh.backHTML = (vlr) => {
-    console.clear();
 
     // vlr = vlr.replace(/\n/g, "<br />");
-   
-
     var vlrTXT = '';
     var alinhamento = '';
+
+    let md = new Remarkable();
+    vlr = md.render(vlr);
+
+
     for (let i = 0; i < wmh.marcDown.length; i++) {
         console.log(vlr.indexOf(wmh.marcDown[i]));
         if (vlr.indexOf(wmh.marcDown[i]) != -1) {
-            
-       
+
+
             switch (wmh.marcDown[i]) {
                 case '|-|-|':
                     alinhamento = 'center';
@@ -96,20 +136,20 @@ wmh.backHTML = (vlr) => {
                     alinhamento = 'right';
 
                     break;
-            
+
                 default:
                     break;
             }
 
             let arr = vlr.split(wmh.marcDown[i]);
             let act = 0;
-        
-        
-        for (let z = 0; z < arr.length; z++) {
+
+
+            for (let z = 0; z < arr.length; z++) {
                 if (act == 0) {
                     if (arr.length - 1 != z) {
-                        vlrTXT += arr[z] + '<p class="whm-' + alinhamento+'">';
-                    }else{
+                        vlrTXT += arr[z] + '<p class="whm-' + alinhamento + '">';
+                    } else {
                         vlrTXT += arr[z]
                     }
                     act = 1;
@@ -120,18 +160,17 @@ wmh.backHTML = (vlr) => {
                     } else {
                         vlrTXT += arr[z]
                     }
-                    act = 0;       
+                    act = 0;
                 }
             }
             vlr = vlrTXT;
             vlrTXT = '';
         }
     }
-    console.log(vlr);
-    let md = new Remarkable();
-    let vlrRec = md.render(vlr);
-    console.log(vlrRec);
-    $('#md').html(vlrRec)
+
+
+
+    return vlr;
 }
 
 wmh.marcDown = [
@@ -139,15 +178,3 @@ wmh.marcDown = [
     '|-||',
     '||-|'
 ]
-
-
-// if (act == 0) {
-//     console.log(arr.length - 1 + '!=' + z);
-//     if (arr.length - 1 != z) {
-//         vlrTXT += arr[z] + '<p class="whm-center">';
-//     }
-//     act = 1;
-// } else {
-//     vlrTXT += arr[z] + '</p>';
-//     act = 0;
-// }
